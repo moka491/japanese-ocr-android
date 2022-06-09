31e7e3376406afcbd96037e4538c85186d4ddaf9
Jannis Becker <jbecker@jannisbecker.me>
2022-06-09 16:56:15 +0200

    refactor: move camera logic to viewmodel

--- app/src/main/java/moe/mokacchi/japaneseocr/ui/screens/camera/CameraScreen.kt
+++ app/src/main/java/moe/mokacchi/japaneseocr/ui/screens/camera/CameraScreen.kt
@@ -1,101 +1,39 @@
 package moe.mokacchi.japaneseocr.ui.screens.camera
 
-import android.annotation.SuppressLint
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.mlkit.vision.MlKitAnalyzer
-import androidx.camera.view.CameraController
-import androidx.camera.view.CameraController.COORDINATE_SYSTEM_VIEW_REFERENCED
-import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.navigation.NavController
-import com.google.mlkit.vision.text.Text
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.japanese.JapaneseTextRecognizerOptions
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import moe.mokacchi.japaneseocr.ui.components.CameraControls
 import moe.mokacchi.japaneseocr.ui.components.CameraOCRBoundaryOverlay
 import moe.mokacchi.japaneseocr.ui.components.CameraPreview
-import org.koin.androidx.compose.get
 import org.koin.androidx.compose.inject
-import java.io.File
-import java.util.concurrent.Executors
 
 @Composable
-@SuppressLint("UnsafeOptInUsageError")
 fun CameraScreen(
     navController: NavController,
 ) {
     val viewModel: CameraScreenViewModel by inject()
 
-    var textBlocks: List<Text.TextBlock> by remember { mutableStateOf(emptyList()) }
-
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
-    val cameraController = get<LifecycleCameraController>()
-    val cameraExecutor = remember { Executors.newSingleThreadExecutor() }
-    val textRecognizer =
-        remember { TextRecognition.getClient(JapaneseTextRecognizerOptions.Builder().build()) }
-    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(true) {
-        with(cameraController) {
-            bindToLifecycle(lifecycleOwner)
-
-            setImageAnalysisAnalyzer(
-                cameraExecutor,
-                MlKitAnalyzer(
-                    listOf(textRecognizer),
-                    COORDINATE_SYSTEM_VIEW_REFERENCED,
-                    cameraExecutor
-                ) {
-                    val res = it.getValue(textRecognizer)
-
-                    if (res != null) {
-                        textBlocks = res.textBlocks
-                    }
-                })
+        with(viewModel) {
+            bindCameraLifecycle(lifecycleOwner)
+            setupAnalyzer()
         }
     }
 
-    CameraPreview(cameraController, modifier = Modifier.fillMaxSize())
-    CameraOCRBoundaryOverlay(textBlocks, modifier = Modifier.fillMaxSize())
+    CameraPreview(viewModel.cameraController, modifier = Modifier.fillMaxSize())
+    CameraOCRBoundaryOverlay(viewModel.textBlocks, modifier = Modifier.fillMaxSize())
     CameraControls {
-        val outputFileOptions =
-            ImageCapture.OutputFileOptions.Builder(
-                File("${context.externalCacheDir}${File.separator}${System.currentTimeMillis()}.png")
-            ).build()
-
-        cameraController.takePicture(
-            outputFileOptions,
-            cameraExecutor,
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    coroutineScope.launch {
-
-                        val result = viewModel.generateOCRResult(
-                            outputFileResults.savedUri.toString(),
-                            textBlocks
-                        )
-
-                        val resultId = viewModel.saveResult(result)
-
-                        withContext(Dispatchers.Main) {
-                            navController.navigate("result/$resultId")
-                        }
-                    }
-                }
-
-                override fun onError(exception: ImageCaptureException) {}
-            })
+        viewModel.takePictureAndStoreResult(context) { resultId ->
+            navController.navigate("result/$resultId")
+        }
     }
 }
 
--- app/src/main/java/moe/mokacchi/japaneseocr/ui/screens/camera/CameraScreenViewModel.kt
+++ app/src/main/java/moe/mokacchi/japaneseocr/ui/screens/camera/CameraScreenViewModel.kt
@@ -1,20 +1,99 @@
 package moe.mokacchi.japaneseocr.ui.screens.camera
 
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.mlkit.vision.MlKitAnalyzer
+import androidx.camera.view.CameraController.COORDINATE_SYSTEM_VIEW_REFERENCED
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.japanese.JapaneseTextRecognizerOptions
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import moe.mokacchi.japaneseocr.domain.model.OCRResult
 import moe.mokacchi.japaneseocr.domain.model.RectangleRegion
 import moe.mokacchi.japaneseocr.domain.model.TextBlock
 import moe.mokacchi.japaneseocr.domain.model.TextLine
 import moe.mokacchi.japaneseocr.domain.usecases.SaveOCRResultUseCase
+import org.koin.androidx.compose.get
 
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.io.File
+import java.util.concurrent.Executors
 
 class CameraScreenViewModel : ViewModel(), KoinComponent {
+
+    val cameraController: LifecycleCameraController by inject()
+    private val cameraExecutor = Executors.newSingleThreadExecutor()
+    private val textRecognizer =
+        TextRecognition.getClient(JapaneseTextRecognizerOptions.Builder().build())
+
     private val saveOCRResultUseCase: SaveOCRResultUseCase by inject()
 
-    fun generateOCRResult(imagePath: String, textBlocks: List<Text.TextBlock>): OCRResult {
+    var textBlocks: List<Text.TextBlock> by mutableStateOf(emptyList())
+
+    fun bindCameraLifecycle(lifecycleOwner: LifecycleOwner) {
+        cameraController.bindToLifecycle(lifecycleOwner)
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    fun setupAnalyzer() {
+        cameraController.setImageAnalysisAnalyzer(
+            cameraExecutor,
+            MlKitAnalyzer(
+                listOf(textRecognizer),
+                COORDINATE_SYSTEM_VIEW_REFERENCED,
+                cameraExecutor
+            ) {
+                val res = it.getValue(textRecognizer)
+
+                if (res != null) {
+                    textBlocks = res.textBlocks
+                }
+            })
+    }
+
+    fun takePictureAndStoreResult(context: Context, onResultSavedCallback: (Int) -> Unit) {
+        takePicture(context) { imageUri ->
+            val result = generateOCRResult(imageUri)
+
+            viewModelScope.launch {
+                val resultId = saveOCRResultUseCase.invoke(result)
+                onResultSavedCallback(resultId)
+            }
+        }
+    }
+
+    private fun takePicture(context: Context, onPictureTaken: (String) -> Unit) {
+        val outputFileOptions =
+            ImageCapture.OutputFileOptions.Builder(
+                File("${context.externalCacheDir}${File.separator}${System.currentTimeMillis()}.png")
+            ).build()
+
+        cameraController.takePicture(
+            outputFileOptions,
+            cameraExecutor,
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    onPictureTaken(outputFileResults.savedUri.toString())
+                }
+
+                override fun onError(exception: ImageCaptureException) {}
+            })
+    }
+
+    private fun generateOCRResult(imagePath: String): OCRResult {
         return OCRResult(
             imagePath,
             textBlocks.filter { block -> block.boundingBox != null }.map { block ->
@@ -39,9 +118,4 @@ class CameraScreenViewModel : ViewModel(), KoinComponent {
                     })
             })
     }
-
-    suspend fun saveResult(ocrResult: OCRResult): Int {
-        return saveOCRResultUseCase.invoke(ocrResult)
-    }
-
 }
